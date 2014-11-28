000057ab500f54cd61731eaea784dc658179d2af
Megan Eborn <mneborn@gmail.com>
2014-11-27 18:29:23 -0700

    add quote page

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
 					<li><a href="about.php">ABOUT</a></li>
 					<li><a href="services.php">SERVICES</a></li>
 					<li><a href="gallery.php">GALLERY</a></li>
-					<li>QUOTE</li>
+					<li><a href="quote.php">QUOTE</a></li>
 					<li><a href="contact.php">CONTACT</a></li>
 				</ul>
 			</div>
---
+++ quote.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Quote | Upholstery Doctor</title>
+		<link rel="stylesheet" href="style/style.css">
+		<link rel="stylesheet" href="style/quote-style.css">
+	</head>
+	<body>
+		<div id="quote-container">
+			<div id="header-container">
+				<?php include('includes/header.php'); ?>
+				<div id="header-intro">
+					<h3>Get A Quote.</h3>
+				</div>
+			</div>
+			<div id="content">
+				
+			</div>
+		</div>
+	</body>
+</html>
